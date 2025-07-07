be4ede6b0b4a18c6c532df1955efb0874026c8e9
vaishnaviterdal <vaishnaviterdal17@gmail.com>
2025-07-07 21:19:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function showMessage() {
+    alert("Welcome to my website, Vaishnavi!");
+}
+
+function changeGreeting() {
+    document.getElementById("greeting").innerText = "You're learning JavaScript 🎉";
+}
+
+function changeBackground() {
+    document.body.style.backgroundColor = "#ffe4e1";
+}
+
+function showAnime() {
+    document.getElementById("anime-name").innerText = "My favorite anime is Demon Slayer 🌸";
+}
+
+function changeHeadingColor() {
+    document.getElementById("main-heading").style.color = "purple";
+}
+
+function hideParagraph() {
+    document.getElementById("hide-me").style.display = "none";
+}
